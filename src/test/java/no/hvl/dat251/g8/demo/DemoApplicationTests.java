888e77809b380bd7a7bbb57428c46b38b8302d7b
Kristoffer <31278784+krnome@users.noreply.github.com>
2021-02-16 15:35:27 +0100

    test

---
+++ src/test/java/no/hvl/dat251/g8/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.dat251.g8.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
